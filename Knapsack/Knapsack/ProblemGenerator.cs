0bcbf664a31137eacd22ff091f512ce315a36393
rinkako <jiajidi@163.com>
2016-11-09 20:45:17 +0800

    init

---
+++ Knapsack/Knapsack/ProblemGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Knapsack
+{
+	/// <summary>
+	/// 生成背包实验数据的类
+	/// </summary>
+	class ProblemGenerator
+	{
+
+	}
+}
